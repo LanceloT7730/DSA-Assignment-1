36e4e0730ca6bd252efde07403596ba8e257c4a7
210046 <ac2952602@akfauniversity.org>
2022-12-03 17:30:24 +0500

    Initial Commit

---
+++ assignment-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment-1
+//
+//  Created by DONIYORBEK IBROKHIMOV on 03/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
