1503aad61dc69d59e9e3265be70bcb8f548c0e16
210046 <ac2952602@akfauniversity.org>
2022-12-03 18:06:48 +0500

    insertHusbandAtLast and printAll functions added

--- assignment-1/main.cpp
+++ assignment-1/main.cpp
@@ -7,8 +7,65 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+struct Node {
+    Node *next, *prev;
+    int age;
+    string name;
+
+    Node() {
+        next = nullptr;
+        prev = nullptr;
+        age = 0;
+    }
+};
+
+class Husband {
+private:
+    Node *husbandHead, *husbandLast;
+
+public:
+    Husband() {
+        husbandHead = nullptr;
+        husbandLast = nullptr;
+    }
+
+    void insertHusbandAtLast(string husband_name, int husband_age) {
+        Node *newHusband = new Node;
+        newHusband->age = husband_age;
+        newHusband->name = husband_name;
+
+        if (husbandHead) {
+            newHusband->next = husbandHead;
+            newHusband->prev = husbandHead;
+            husbandHead = newHusband;
+            husbandLast = newHusband;
+        } else {
+            newHusband->next = husbandLast->next;
+            husbandLast->next = newHusband;
+            newHusband->prev = husbandLast;
+            husbandLast = newHusband;
+        }
+    }
+
+    void printAll() {
+        Node *temp = husbandHead;
+        int i = 1;
+        while (temp) {
+            cout << "Name " << i << " " << temp->name
+                 << "\nAge " << i << " " << temp->age << endl;
+            temp = temp->next;
+            i++;
+        }
+    }
+
+};
+
+int main() {
+    Husband husband1;
+    husband1.insertHusbandAtLast("h1", 34);
+    husband1.printAll();
+
     return 0;
 }
